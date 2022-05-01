795f2ea1aeab7fabe361c2b89997b3a3f8d22c8f
Bryan Boh <Bryanboh@gmail.com>
2022-05-01 20:20:41 +0800

    Halo boobing

--- Assets/Scripts/Halo.cs
+++ Assets/Scripts/Halo.cs
@@ -6,15 +6,30 @@ public class Halo : MonoBehaviour
 {
     // Start is called before the first frame update
     float ogLocalY;
+    Transform halo;
+    float timer;
+    float targetYPos;
+
+    float duration = 0.5f;
     void Start()
     {
-        ogLocalY = this.gameObject.transform.localPosition.y;
+        timer = duration;
+        halo = gameObject.transform;
+        ogLocalY = halo.localPosition.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.gameObject.transform.localScale.Set(this.gameObject.transform.localPosition.x,
-            Random.Range(ogLocalY - 10f, ogLocalY + 10f), this.gameObject.transform.localPosition.z);
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            targetYPos = Random.Range(ogLocalY - 0.2f, ogLocalY + 0.2f);
+            timer = duration;
+        }
+        halo.localPosition = Vector3.Lerp(halo.localPosition, 
+            new Vector3(halo.localPosition.x, targetYPos, halo.localPosition.z), duration - timer);
+
     }
 }
